10682ca95d3cf23aa4d9f32a57c2f6da2c7ea76c
Naomi Navkal <navkal@hotmail.com>
2016-08-02 11:54:11 -0400

    Fix footer

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <div class="navbar navbar-default navbar-fixed-bottom" style="padding-top:10px">
   <div class="container">
-    <p class="text-muted" style="text-align:center"><?php echo $footer; ?></p>
+    <p class="text-muted" style="text-align:center"><?php global $footer; echo $footer; ?></p>
   </div>
 </div>
+
--- util.php
+++ util.php
@@ -29,15 +29,16 @@
     echo $message . "<br/>";
   }
 
-  function downloadFile( $file )
+  function downloadFile( $filename )
   {
     header( 'Content-Description: File Transfer' );
     header( 'Content-Type: application/octet-stream' );
-    header( 'Content-Disposition: attachment; filename="' . basename( $file ) . '"' );
+    header( 'Content-Disposition: attachment; filename="' . basename( $filename ) . '"' );
     header( 'Expires: 0' );
     header( 'Cache-Control: must-revalidate' );
     header( 'Pragma: public' );
-    header( 'Content-Length: ' . filesize( $file ) );
-    readfile( $file );
+    header( 'Content-Length: ' . filesize( $filename ) );
+    readfile( $filename );
   }
+
 ?>
