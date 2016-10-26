ec3dd945ee473b61e5766405cdcd769569713745
Naomi Navkal <navkal@hotmail.com>
2016-10-26 14:56:59 -0400

    Lato font family

--- footer.php
+++ footer.php
@@ -2,7 +2,6 @@
 
 <div class="navbar navbar-default navbar-fixed-bottom" style="padding-top:10px">
   <div class="container">
-    <p class="text-muted" style="text-align:center"><?php global $footer; echo $footer; ?></p>
+    <p class="text-muted lato" style="text-align:center"><?php global $footer; echo $footer; ?></p>
   </div>
 </div>
-
