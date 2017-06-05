e9dfc5ae7279c053848273438b2322f7168005db
Naomi Navkal <navkal@hotmail.com>
2017-06-05 18:19:55 -0400

    Change mechanism for identifying navbar.csv

--- util.php
+++ util.php
@@ -3,7 +3,7 @@
 
   session_start();
 
-  function initUi( $navbarCsvLocation = "" )
+  function initUi()
   {
     global $siteName;
     global $footer;
@@ -20,7 +20,8 @@
       $navbarHideItems = explode( ",", $navbarHideEnv );
     }
 
-    $file = fopen( $navbarCsvLocation . "navbar.csv", "r" );
+    $navbarCsv = isset( $_SESSION['navbarCsv'] ) ? $_SESSION['navbarCsv'] : $_SERVER["DOCUMENT_ROOT"] . "/navbar.csv";
+    $file = fopen( $navbarCsv, "r" );
     $siteName = fgetcsv( $file )[0];
     $footer = fgetcsv( $file )[0];
     $mailto = fgetcsv( $file )[0];
