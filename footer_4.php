9b2286c112f4af2dc2ff8d57bf5967b0a302ddc4
navkal <navkal@hotmail.com>
2018-10-19 22:52:47 -0400

    Migrate from Bootstrap 3 to Bootstrap 4

--- footer_4.php
+++ footer_4.php
@@ -1,14 +1,12 @@
 <!-- Copyright 2018 Energize Apps.  All rights reserved. -->
-<div class="navbar navbar-default navbar-fixed-bottom" style="padding-top:10px">
-  <div class="container">
-    <p class="text-muted lato" style="text-align:center">
-      <span style="padding-right: 1em;">
-        <?php global $footer; echo $footer; ?>
-      </span>
-      <a href="mailto:<?php global $mailto; echo $mailto; ?>">
-        <i class="fas fa-envelope" title="<?=$mailto?>">
-        </i>
-      </a>
-    </p>
-  </div>
+<div class="navbar fixed-bottom navbar-light bg-light" style="border-top:1px solid lightgray;" >
+  <p class="text-muted lato" style="margin:auto; font-size:14px" >
+    <span style="padding-right: 1em;">
+      <?php global $footer; echo $footer; ?>
+    </span>
+    <a href="mailto:<?php global $mailto; echo $mailto; ?>">
+      <i class="fas fa-envelope" title="<?=$mailto?>">
+      </i>
+    </a>
+  </p>
 </div>
